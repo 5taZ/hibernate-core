779184cd89a6b7cf774effe48ff8af2aaa6eee56
5taZ <stasyfreak@gmail.com>
2025-11-30 22:36:01 +0300

    Created config

---
+++ src/main/java/services/HibernateConfiguration.java
@@ -0,0 +1,32 @@
+package services;
+
+import entity.Client;
+import entity.Coupon;
+import entity.Order;
+import entity.Profile;
+import org.hibernate.SessionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HibernateConfiguration {
+    @Bean
+    public SessionFactory sessionFactory() {
+        org.hibernate.cfg.Configuration configuration = new org.hibernate.cfg.Configuration();
+        configuration
+                .addPackage("java")
+                .addAnnotatedClass(Client.class)
+                .addAnnotatedClass(Coupon.class)
+                .addAnnotatedClass(Order.class)
+                .addAnnotatedClass(Profile.class)
+                .setProperty("hibernate.connection.driver_class", "org.postgresql.Driver")
+                .setProperty("hibernate.connection.url", "jdbc:postgresql://localhost:5432/postgres")
+                .setProperty("hibernate.connection.username", "postgres")
+                .setProperty("hibernate.connection.password", "root")
+                .setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect")
+                .setProperty("hibernate.show_sql", "true")
+                .setProperty("hibernate.hbm2ddl.auto", "update");
+
+        return configuration.buildSessionFactory();
+    }
+}
