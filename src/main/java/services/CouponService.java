c8fbb8a445b85739f2b1121ed7bd2c42ed9ecf80
5taZ <stasyfreak@gmail.com>
2025-11-30 23:24:00 +0300

    Added Coupone Service

---
+++ src/main/java/services/CouponService.java
@@ -0,0 +1,39 @@
+package services;
+
+import components.TransactionHelper;
+import entity.Coupon;
+import org.hibernate.SessionFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CouponService {
+    private final SessionFactory sessionFactory;
+    private final TransactionHelper transactionHelper;
+
+    public CouponService(SessionFactory sessionFactory, TransactionHelper transactionHelper) {
+        this.sessionFactory = sessionFactory;
+        this.transactionHelper = transactionHelper;
+    }
+
+    public Coupon saveCoupon(Coupon coupon){
+        return transactionHelper.executeInTransaction(session -> {
+            session.persist(coupon);
+            return coupon;
+        });
+    }
+
+    public void updateCoupon(Long id, String code, Double discount){
+        transactionHelper.executeInTransaction(session -> {
+            Coupon coupon = session.get(Coupon.class, id);
+            if (coupon != null){
+                coupon.setCode(code);
+                coupon.setDiscount(discount);
+            }
+        });
+    }
+    public Coupon findCoupon(Long id){
+        return transactionHelper.executeInTransaction(session -> {
+            return session.get(Coupon.class, id);
+        });
+    }
+}
